69ba55f228facdc0289fbc64360713f0529bb4c4
icarolima <icarolima@lsd.ufcg.edu.br>
2018-12-04 00:18:14 -0300

    Refatora até a 16

--- Part3.sql
+++ Part3.sql
@@ -10,6 +10,7 @@ AS
                            FROM   PROJETO
                            WHERE  dt_inicio > '12/01/2012');                      
 
+
 -- 2. Liste a quantidade de alunos de mestrado que possuem bolsa, por Linha de
 -- Pesquisa.
 SELECT COUNT(*),
@@ -19,6 +20,7 @@ FROM   ALUNO
 WHERE  LOWER(ALUNO.nivel) = 'mestrado'
 GROUP  BY cod_cnpq,
           cod_sub_cnpq;
+
 
 -- 3. Crie uma view que lista os laboratórios que executam projetos iniciados
 -- em 2005.
@@ -37,6 +39,7 @@ AS
                                                     = 2005
                                             ));
 
+
 -- 4. Quais os alunos que de doutorado nasceram depois de 1990 e têm alguma
 -- publicação no ano de 2012?
 SELECT ALUNO.*
@@ -49,12 +52,14 @@ WHERE  EXTRACT (year FROM ALUNO.dt_nasc) > 1990
        AND aluno_pub.cod_publicacao = pub.codigo
        AND pub.ano = 2012;
 
+
 -- 5. Quais os projetos que iniciaram depois de 2012 e possuem um orçamento
 -- menor que R$ 800.000?
 SELECT *
 FROM   PROJETO
 WHERE  EXTRACT (year FROM dt_inicio) > 2012
        AND orcamento < 800000;
+
 
 -- 6. Quais professores participam de todos os projetos?
 SELECT prof.*
@@ -63,6 +68,7 @@ WHERE  (SELECT COUNT(*)
         FROM   PROFESSOR_PROJETO prof_proj
         WHERE  prof.matricula = prof_proj.mat_professor) = (SELECT COUNT(*)
                                                             FROM   PROJETO);
+
 
 -- 7. Quais agências financiadoras que financiam bolsas de mais de R$2000 para
 --alunos de doutorado?
@@ -85,6 +91,7 @@ WHERE  dept.mat_professor = prof.matricula
                                   FROM   ALUNO
                                   WHERE  LOWER(nivel) = 'doutorado');
 
+
 -- 9. Liste a quantidade de alunos de graduação financiados por agência
 -- financiadora, exiba todos os dados da agência, inclua as agências que não
 -- financiam nenhum aluno graduação.
@@ -94,6 +101,7 @@ SELECT (SELECT COUNT(*)
                AND LOWER(ALUNO.nivel) = 'graduação') AS alunos_financiados,
        agencia.*
 FROM   AGENCIA_FINANCIADORA agencia;
+
 
 -- 10. Liste os departamentos por tamanho do orçamento dos projetos coordenados
 -- pelos seus professores.
@@ -106,7 +114,8 @@ SELECT (SELECT SUM(proj.orcamento)
                                       WHERE  codigo = dept.codigo)),
        dept.*
 FROM   DEPARTAMENTO dept;
- 
+
+
 -- 11.Quais os alunos de doutorado que participaram de alguma publicação em
 -- 2011?
 SELECT *
@@ -121,6 +130,7 @@ WHERE  ALUNO.matricula IN (SELECT ALUNO_PUBLICACAO.mat_aluno
                                   PUBLICACAO.ano = 2011))
        AND LOWER(ALUNO.nivel) = 'doutorado';
 
+
 -- 12.Liste a quantidade de publicações no ano de 2005 que não contam com a
 -- participação nenhum aluno de mestrado.
 SELECT COUNT(*)
@@ -133,51 +143,55 @@ WHERE  PUBLICACAO.ano = 2005
                                                          WHERE
                                         LOWER(ALUNO.nivel) <> 'mestrado'));
 
+
 -- 13.Qual a soma dos orçamentos dos projetos que encerraram em 2009.
 SELECT SUM(orcamento)
 FROM   PROJETO
 WHERE  EXTRACT (year FROM dt_fim) = 2009;
 
-/* Questão 14 */
 
+-- 14.Liste o nível e o nome dos alunos que participaram de publicações depois
+-- de 2012.
 SELECT nivel,
        nome
-FROM   aluno
-       join aluno_publicacao
-         ON aluno.matricula = aluno_publicacao.mat_aluno
-       join publicacao
-         ON publicacao.codigo = aluno_publicacao.cod_publicacao
-WHERE  publicacao.ano > 2012; 
+FROM   ALUNO
+       join ALUNO_PUBLICACAO
+         ON ALUNO.matricula = ALUNO_PUBLICACAO.mat_aluno
+       join PUBLICACAO
+         ON PUBLICACAO.codigo = ALUNO_PUBLICACAO.cod_publicacao
+WHERE  PUBLICACAO.ano > 2012;
+
 
 -- 15. Crie um trigger para toda vez que uma nova patente for inserida,
 -- incremente a coluna premiação (que deve ser colocada com valor ZERO para cada
 -- projeto cadastrado) na tabela de projetos.
 CREATE OR replace TRIGGER incr_premiacao_por_patente
-  BEFORE INSERT ON patente
+  BEFORE INSERT ON PATENTE
   FOR EACH ROW
 BEGIN
-    UPDATE projeto
+    UPDATE PROJETO
     SET    premiacao = premiacao + 1
     WHERE  codigo = :new.cod_projeto;
 END;
 
 /
 
+
 -- 16. Crie um trigger que não permita a inserção de bolsas com valor menor que
 -- R$ 430 para alunos de graduação e R$ 1000 para alunos de mestrado e
 -- doutorado.
 CREATE OR replace TRIGGER bolsa_restricoes
-  BEFORE INSERT ON aluno
+  BEFORE INSERT ON ALUNO
   FOR EACH ROW
 BEGIN
-    IF Lower(:new.nivel) = 'graduação'
+    IF LOWER(:new.nivel) = 'graduação'
        AND :new.valor_bolsa < 430 THEN
-      Raise_application_error(-20001,
+      RAISE_APPLICATION_ERROR(-20001,
       'Aluno de graduação não pode possuir bolsa menor que R$ 430.');
-    ELSIF ( Lower(:new.nivel) = 'mestrado'
-             OR Lower(:new.nivel) = 'doutorado' )
+    ELSIF ( LOWER(:new.nivel) = 'mestrado'
+             OR LOWER(:new.nivel) = 'doutorado' )
           AND :new.valor_bolsa < 1000 THEN
-      Raise_application_error(-20002,
+      RAISE_APPLICATION_ERROR(-20002,
 'Aluno de mestrado ou doutorado não pode possuir bolsa menor que R$ 1000.');
 END IF;
 END;
